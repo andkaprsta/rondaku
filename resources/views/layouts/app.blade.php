e515471862eda0d6ebe18d11e73b23da89220c74
andkaprsta <andikaprasetiarafi@gmail.com>
2026-08-10 09:43:31 +0700

    pwa test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
     <meta name="theme-color" content="#2563EB">
+
+    <link rel="manifest" href="/manifest.webmanifest">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
